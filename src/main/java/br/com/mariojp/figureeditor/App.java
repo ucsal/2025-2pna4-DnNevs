a68504c9f1a47ca8c8806db8dacc9c0ce6cbf0e7
DnNevs <daniel.neves@ucsal.edu.br>
2025-09-11 19:24:48 -0300

    feat: Feito alterações na classe App para adição de ideias de evolução

--- src/main/java/br/com/mariojp/figureeditor/App.java
+++ src/main/java/br/com/mariojp/figureeditor/App.java
@@ -6,19 +6,39 @@ import java.awt.*;
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception ignored) {}
+            JFrame frame = new JFrame("Editor de figura");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            JFrame frame = new JFrame("Figure Editor — Clique para inserir figuras");
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
             DrawingPanel panel = new DrawingPanel();
 
-            frame.setLayout(new BorderLayout());
-            frame.add(panel, BorderLayout.CENTER);
+            JButton btCor = new JButton("Cor...");
+            btCor.addActionListener(e -> {
+                Color novaCor = JColorChooser.showDialog(frame, "Escolha a cor",Color.blue);
+                if (novaCor != null) {
+                    panel.setCurrentColor(novaCor);
+                }
+            });
 
-            frame.setSize(900, 600);
+            JButton btClear = new JButton("Clear");
+            btClear.addActionListener(e -> panel.clear());
+
+            JButton btShape = new JButton("Retângulo");
+            btShape.addActionListener(e -> {
+                boolean isCircle = panel.toggleShapeType();
+                btShape.setText(isCircle ? "Circulo" : "Retângulo");
+            });
+
+            JPanel barra = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            barra.add(btShape);
+            barra.add(btCor);
+            barra.add(btClear);
+
+            frame.getContentPane().setLayout(new BorderLayout());
+            frame.getContentPane().add(panel, BorderLayout.CENTER);
+            frame.getContentPane().add(barra, BorderLayout.NORTH);
+
+            frame.setSize(800, 600);
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
